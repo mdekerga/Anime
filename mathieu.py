edb3bf8fe2fc2098c22421710756cf88321037bf
mdekerga <mathieu.dekergariou@gmail.com>
2025-12-01 08:26:53 +0100

    cleaning process

--- mathieu.py
+++ mathieu.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 
@@ -6,61 +7,57 @@ file = "popular_anime.csv"
 
 df = pd.read_csv(file)
 
+#Nombre de lignes avant le nettoyage
+print(df.shape)
+
 #Nombre de valeurs sans colonnes
 print("\n Missing Values (per column):")
 print(df.isnull().sum())
 
-#colonnes avec valeurs manquantes : genres, type, episodes, aired_from, aired_to, score, scored_by, rank, rating, studios, producers, trailer, synopsis
-
-
 #Remplissage des colonnes vides
-
 df["genres"] = df["genres"].fillna("Other")
 df["episodes"] = df["episodes"].fillna(1)
 
-
-
 #suppression des colonnes inutiles
-
 colonne_a_drop = ['synopsis','trailer','producers']
-df.drop(columns=colonne_a_drop)
+df = df.drop(columns=colonne_a_drop)
 
+#suppression de ligne 
+df = df.dropna(subset=['score', 'episodes', 'scored_by', 'rank',"rating","studios"])
 
-print(df["aired_from"])
+#Suppression des duplications
+df = df.drop_duplicates()
 
+#réinitilise l'index 
+df.reset_index(drop=True, inplace=True)
 
-duplicate = df[df.duplicated()]
+df["aired_from"] = df["aired_from"].fillna(pd.NaT)
+df['aired_to'] = df['aired_to'].fillna(pd.NaT)
 
+#Création de nouvelle colonne
+def get_season_anime(date):
+    if(pd.isna(date)):
+        return np.nan
 
+    date = pd.to_datetime(date)
 
-print("Duplicate Rows :")
+    month = date.month
 
-# Print the resultant Dataframe
-print(duplicate["name"])
+    season_index = (month - 1) // 3 
+        
+    seasons = ["Winter", "Spring", "Summer", "Fall"]
+        
+    return seasons[season_index]
+
+print(get_season_anime("2009-04-05T00:00:00+00:00"))
+
+df["season_aired"] = df["aired_from"].apply(get_season_anime)
 
 #Nombre de lignes après le nettoyage
 print(df.isnull().sum())
 print(df.shape)
 
 
-df["aired_from"] = df["aired_from"].fillna("Unknown")
-
-#Création de nouvelle colonne
-def get_season_anime(date):
-    if(date != "Unknown"):
-        date = pd.to_datetime(date)
-
-        month = date.month
-
-        season_index = (month - 1) // 3 
-        
-        seasons = ["Winter", "Spring", "Summer", "Fall"]
-        
-        return seasons[season_index]
-
-print(get_season_anime("2009-04-05T00:00:00+00:00"))
-
-df["season_aired"] = df["aired_from"].apply(get_season_anime)
 
 #recherche de corrélation entre les variables
 #print(df.corr())
