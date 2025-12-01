ee3b79c8cc7149d0fd3dd072d77556411bddf2e4
Matheo <babouin.panda@gmail.com>
2025-12-01 14:54:21 +0100

    tkt

--- matheo.py
+++ matheo.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 
+# ====================================================================
+# --- Cleaning ---
+# ====================================================================
 
 df = pd.read_csv("C:/Users/babou/Downloads/AnimeProject/Anime/popular_anime.csv")
 #Nombre de lignes avant le nettoyage
@@ -40,11 +43,25 @@ df["season_aired"] = df["aired_from"].apply(get_season_anime)
 print(df.isnull().sum())
 print(df.shape)
 
-# Pour analyse des 10%, créer un subset avec uniquement les lignes contenant des valeurs de score.
+# ====================================================================
+# --- Création du Subset 10% ---
+# ====================================================================
+
+# Pour analyse des 10%, création d'un subset avec uniquement les lignes contenant des valeurs de score.
 score_anime = df.dropna(subset=['score', 'scored_by']).copy()
 # Permet d'afficher plus que d'habitude
-pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_rows', None)
 # Subset des 10% animés avec le plus haut score
 top10pourcent = score_anime.head(int(len(score_anime) * 0.10)).copy()
-print(top10pourcent['score'])
+print(top10pourcent)
+# Détermination du seuil pour etre dans les 10 pourcents
+seuil = top10pourcent['score'].min()
+print(seuil)
 
+# Créer une colonne is_top_10
+df['is_top_10'] = df.index.isin(top10pourcent.index).astype(int)
+print(df)
+
+#Nombre de valeurs sans colonnes
+print("\n Missing Values (per column):")
+print(df.isnull().sum())
